f148fc66561b161b2dbc748a480cc4247a7355ac
pravinkumar <pravinbairwa584@gmail.com>
2025-01-03 21:04:34 +0530

    Updated CSS and JS files for responsive movie dashboard

--- responsive-movie-dashboard-main - Copy/responsive-movie-dashboard-main/assets/js/main.js
+++ responsive-movie-dashboard-main - Copy/responsive-movie-dashboard-main/assets/js/main.js
@@ -1,38 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const sliders = document.querySelectorAll(".image-slider");
-
-//     sliders.forEach((slider) => {
-//         const sliderWrapper = slider.querySelector(".slider-wrapper");
-//         const sets = slider.querySelectorAll(".slider-set");
-//         const prevButton = slider.querySelector(".prev-btn");
-//         const nextButton = slider.querySelector(".next-btn");
-
-//         let currentIndex = 0;
-//         const totalSets = sets.length;
-
-//         function changeSlide() {
-//             sliderWrapper.style.transform = `translateX(-${currentIndex * 100}%)`;
-//             prevButton.disabled = currentIndex === 0;
-//             nextButton.disabled = currentIndex === totalSets - 1;
-//         }
-
-//         prevButton.addEventListener("click", () => {
-//             if (currentIndex > 0) {
-//                 currentIndex--;
-//                 changeSlide();
-//             }
-//         });
-
-//         nextButton.addEventListener("click", () => {
-//             if (currentIndex < totalSets - 1) {
-//                 currentIndex++;
-//                 changeSlide();
-//             }
-//         });
-
-//         changeSlide(); // Initialize the slider state
-//     });
-// });
 
 
 
